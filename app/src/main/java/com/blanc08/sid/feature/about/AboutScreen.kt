d83eb36eb137e4116a031272002ac6d10bd8fdd2
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2024-10-12 23:25:49 +0700

    about page - layout update

--- app/src/main/java/com/blanc08/sid/feature/about/AboutScreen.kt
+++ app/src/main/java/com/blanc08/sid/feature/about/AboutScreen.kt
@@ -1,17 +1,37 @@
 package com.blanc08.sid.feature.about
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.blanc08.sid.designsystem.theme.AppTypography
 
 @Composable
 fun AboutRoute() {
     AboutScreen()
 }
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen() {
+    Column {
+        TopAppBar(
+            title = {
+                Text(
+                    style = AppTypography.titleLarge,
+                    text = "About"
+                )
+            },
+        )
 
-    Text(text = "OK")
-
+        Column(modifier = Modifier.padding(horizontal = 10.dp)) {
+            Text(
+                text = "Ciputri, Kec. Pacet, Kabupaten Cianjur, Jawa Barat 43253"
+            )
+        }
+    }
 }
